a680f2d3645448afbf440c0e0f7fabb068a1e592
Tianyu <tianyu.zhang.18@gmail.com>
2020-06-13 19:24:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+
+######## chatbot ########
+import os
+import re
+import sqlite3
+from collections import Counter
+from string import punctuation
+from math import sqrt
+
+# initialize the connection to the database
+# create the tables needed by the program
+if not os.path.exists('chatbot.sqlite'):
+    connection = sqlite3.connect('chatbot.sqlite')
+    cursor = connection.cursor()
+    create_table_request_list = [
+        'CREATE TABLE words(word TEXT UNIQUE)',
+        'CREATE TABLE sentences(sentence TEXT UNIQUE, used INT NOT NULL DEFAULT 0)',
+        'CREATE TABLE associations (word_id INT NOT NULL, sentence_id INT NOT NULL, weight REAL NOT NULL)',
+    ]
+    for create_table_request in create_table_request_list:
+        try:
+            cursor.execute(create_table_request)
+        except:
+            pass
+else:
+    connection = sqlite3.connect('chatbot.sqlite')
+    cursor = connection.cursor()
+
+def get_id(entityName, text):
+    """Retrieve an entity's unique ID from the database, given its associated text.
+    If the row is not already present, it is inserted.
+    The entity can either be a sentence or a word."""
+    tableName = entityName + 's'
+    columnName = entityName
+    cursor.execute('SELECT rowid FROM ' + tableName + ' WHERE ' + columnName + ' = ?', (text,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    else:
+        cursor.execute('INSERT INTO ' + tableName + ' (' + columnName + ') VALUES (?)', (text,))
+        return cursor.lastrowid
+
+def get_words(text):
+    """Retrieve the words present in a given string of text.
+    The return value is a list of tuples where the first member is a lowercase word,
+    and the second member the number of time it is present in the text."""
+    wordsRegexpString = '(?:\w+|[' + re.escape(punctuation) + ']+)'
+    wordsRegexp = re.compile(wordsRegexpString)
+    wordsList = wordsRegexp.findall(text.lower())
+    return Counter(wordsList).items()
+
+# global B
+B = 'Hello!'
+def response(B, H):
+    # output bot's message
+    # print('B: ' + B)
+    # ask for user input; if blank line, exit the loop
+    # H = input('H: ').strip()
+    if H == '':
+        return
+    # store the association between the bot's message words and the user's response
+    words = get_words(B)
+    words_length = sum([n * len(word) for word, n in words])
+    sentence_id = get_id('sentence', H)
+    for word, n in words:
+        word_id = get_id('word', word)
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO associations VALUES (?, ?, ?)', (word_id, sentence_id, weight))
+    connection.commit()
+    # retrieve the most likely answer from the database
+    cursor.execute('CREATE TEMPORARY TABLE results(sentence_id INT, sentence TEXT, weight REAL)')
+    words = get_words(H)
+    words_length = sum([n * len(word) for word, n in words])
+    for word, n in words:
+        weight = sqrt(n / float(words_length))
+        cursor.execute('INSERT INTO results SELECT associations.sentence_id, sentences.sentence, ?*associations.weight/(4+sentences.used) FROM words INNER JOIN associations ON associations.word_id=words.rowid INNER JOIN sentences ON sentences.rowid=associations.sentence_id WHERE words.word=?', (weight, word,))
+    # if matches were found, give the best one
+    cursor.execute('SELECT sentence_id, sentence, SUM(weight) AS sum_weight FROM results GROUP BY sentence_id ORDER BY sum_weight DESC LIMIT 1')
+    row = cursor.fetchone()
+    cursor.execute('DROP TABLE results')
+    # otherwise, just randomly pick one of the least used sentences
+    if row is None:
+        cursor.execute('SELECT rowid, sentence FROM sentences WHERE used = (SELECT MIN(used) FROM sentences) ORDER BY RANDOM() LIMIT 1')
+        row = cursor.fetchone()
+    # tell the database the sentence has been used once more, and prepare the sentence
+    B = row[1]
+    cursor.execute('UPDATE sentences SET used=used+1 WHERE rowid=?', (row[0],))
+    return B
+######## chatbot end ########
+
+
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    global B
+    try:
+        json['bot_message'] = response(B, json['message'])
+        B = json['bot_message']
+    except:
+        pass
+    print(json)
+    socketio.emit('my response', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
